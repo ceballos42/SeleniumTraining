fe0a5100796bdd91de908721512a31f13fae639b
Ceballos Martinez, Abraham (j.ceballos.martinez) <j.ceballos.martinez@accenture.com>
2023-07-22 18:54:20 -0600

    Adding Edge default Browser

--- src/main/java/helper/DriverFactory.java
+++ src/main/java/helper/DriverFactory.java
@@ -3,15 +3,23 @@ package helper;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 import java.time.Duration;
 
 public class DriverFactory {
 
     public static WebDriver newDriver() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless=true");
-        WebDriver driver = new ChromeDriver(options);
+        //ChromeOptions options = new ChromeOptions();
+        //options.addArguments("headless=true");
+        //WebDriver driver = new ChromeDriver(options);
+
+        //System.setProperty("webdriver.edge.driver", "/selenium/edgedriver/msedgedriver.exe");
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new EdgeDriver(options);
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         return driver;
