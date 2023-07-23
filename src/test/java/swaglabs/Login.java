e2ce43046fa04f837607d4268b0da92e6a1273b1
Ceballos Martinez, Abraham (j.ceballos.martinez) <j.ceballos.martinez@accenture.com>
2023-07-22 20:12:27 -0600

    Extracting Constants

--- src/test/java/swaglabs/Login.java
+++ src/test/java/swaglabs/Login.java
@@ -13,22 +13,27 @@ import static helper.Pages.SWAG_LABS_HOME;
 
 public class Login {
 
+    public static final String USER_NAME_ID = "user-name";
+    public static final String PASSWORD_ID = "password";
+    public static final String SHOPPING_CART_CONTAINER_ID = "shopping_cart_container";
+    public static final String LOGIN_BUTTON_ID = "login-button";
+
     @Test
     public void Login() {
 
         WebDriver driver = DriverFactory.newDriver();
         driver.get(SWAG_LABS_HOME);
 
-        WebElement user_name = driver.findElement(By.id("user-name"));
+        WebElement user_name = driver.findElement(By.id(USER_NAME_ID));
         user_name.sendKeys("standard_user");
 
-        WebElement password = driver.findElement(By.id("password"));
+        WebElement password = driver.findElement(By.id(PASSWORD_ID));
         password.sendKeys("secret_sauce");
 
-        WebElement  login_button = driver.findElement(By.id("login-button"));
+        WebElement  login_button = driver.findElement(By.id(LOGIN_BUTTON_ID));
         login_button.click();
 
-        WebElement shoppingCartIcon = WaitingUtils.waitUntilClickable(driver, By.id("shopping_cart_container"));
+        WebElement shoppingCartIcon = WaitingUtils.waitUntilClickable(driver, By.id(SHOPPING_CART_CONTAINER_ID));
 
 
 
